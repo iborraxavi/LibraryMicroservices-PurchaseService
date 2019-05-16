9431845b9261fd62bd4e88c8d3a3281aebc2733f
Xavier Iborra Sanchez <xiborra@certiorem.com>
2019-05-16 13:52:22 +0200

    Add RestTemplate Bean and add configuration properties

--- src/main/java/com/certiorem/librarymicroservicesproject/application/PurchaseMicroServiceApplication.java
+++ src/main/java/com/certiorem/librarymicroservicesproject/application/PurchaseMicroServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -15,6 +17,11 @@ public class PurchaseMicroServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PurchaseMicroServiceApplication.class, args);
+	}
+	
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
